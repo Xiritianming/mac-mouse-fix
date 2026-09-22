0dc11178a9244048a28c49e5af67b5c12c19d199
Xiritianming <zhonghuaiyong@gmail.com>
2026-09-23 01:48:49 +0800

    sync: absorb upstream ModifiedDragOutputAddMode.m coalescing changes

--- Helper/Core/Drag/AddMode/ModifiedDragOutputAddMode.m
+++ Helper/Core/Drag/AddMode/ModifiedDragOutputAddMode.m
@@ -36,7 +36,7 @@ static BOOL _didConclude;
     return;
 }
 
-+ (void)handleMouseInputWhileInUseWithDeltaX:(double)deltaX deltaY:(double)deltaY event:(CGEventRef _Nullable)event {
++ (void)handleMouseInputWhileInUseWithDeltaX:(double)deltaX deltaY:(double)deltaY {
     
     if (!_didConclude) {
         if (_addModePayload != nil) {
